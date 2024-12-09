2829a2228e78736aee9f0d0b6664dddd2ae55448
micaelleos <micaelle.oliveira10@hotmail.com>
2024-12-09 09:14:27 -0300

    info card about documents

--- page/upload_page.py
+++ page/upload_page.py
@@ -2,6 +2,9 @@ import streamlit as st
 import os
 from src.scripts.chat.document_loader import *
 
+if "documents" not in st.session_state:
+    st.session_state.documents = []
+
 st.title("Faça upload do documento")
 
 uploaded_file = st.file_uploader("Escolha um arquivo PDF",type=['pdf'],accept_multiple_files=False)
@@ -14,10 +17,14 @@ if uploaded_file is not None:
             #load_doc_pipeline()
             load_doc_family_pipeline()
         st.success("Docmuent loaded!")
+        st.session_state.documents.append(uploaded_file.name)
 
     except ValueError as e:
         st.error("O documento está vazio (não possui caracteres). Por favor, faça um novo upload.")
-    
+
+if st.session_state.documents:
+    for doc in st.session_state.documents:
+        st.info(f"Documento {doc} na base de dados")
 
 
 
