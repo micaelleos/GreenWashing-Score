0172920051995963ca0a9ea5f8654180a610850f
Micaelle Souza <61849864+micaelleos@users.noreply.github.com>
2024-12-17 00:56:02 +0000

    reorg pages

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ pages = {
         st.Page("./page/chat_page.py", title="Chat com assistente"),
         st.Page("./page/upload_page.py", title="Upload"),
         st.Page("./page/score_page.py", title="Análise"),
-        st.Page("./page/criterios_page.py", title="Critérios de Análise"),
     ],
     "Recursos": [
          st.Page("./page/about_page.py", title="Sobre"),
+         st.Page("./page/criterios_page.py", title="Critérios de Análise"),
          st.Page("./page/gri_page.py", title="Padrão GRI"),
      ]
 }
